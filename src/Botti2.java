5169a9c854ceee6acc38e03bf0429d8ffd375e76
Matias Saarinen <matias.saarinen@aalto.fi>
2012-11-14 22:36:55 +0200

    Botti2 luotu
    Loin varsinaisen botin, joka periytyy PircBotista. Tähän luokkaan luon
    vielä erittäin helppokäyttöiset metodit, joiden avulla saa lähetettyä
    viestejä ja mahdollisesti tehtyä jotain muuta.

---
+++ src/Botti2.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.jibble.pircbot.PircBot;
+
+public class Botti2 extends PircBot {
+	private List<BottiKuuntelija> kuuntelijat = new ArrayList<BottiKuuntelija>();
+	
+	public Botti2() {
+	}
+	
+	@Override
+	protected void onMessage( String channel, 
+	                          String sender, 
+	                          String login, 
+	                          String hostname, 
+	                          String message ) {
+		this.ilmoitaKuuntelijoille( message, channel, sender );
+	}
+
+	private void ilmoitaKuuntelijoille( String viesti,
+										String kanava,
+										String lahettaja ) {
+		for( BottiKuuntelija kuuntelija : this.kuuntelijat ) {
+			kuuntelija.uusiViesti( viesti, kanava, lahettaja );
+		}
+		
+	}
+	
+	public void lisaaBottiKuuntelija( BottiKuuntelija bk ) {
+		this.kuuntelijat.add( bk );
+		this.jarjestaKuuntelijat();
+	}
+
+	private void jarjestaKuuntelijat() {
+		Collections.sort( this.kuuntelijat );
+	}
+}
