3e1455d68179bfbd5714a49d27a789f358344eca
Matias Saarinen <matias.saarinen@aalto.fi>
2012-11-14 22:28:50 +0200

    Parannuksia Abstraktiin luokkaan
    Parantelin luokkaa, jotta sen käyttäminen olisi helpompaa. Huomaa, että
    viittaus bottiin on nyt tallennettuna attribuuttiin botti.

--- src/AbstraktiBottiKuuntelija.java
+++ src/AbstraktiBottiKuuntelija.java
@@ -1,13 +1,30 @@
 
 public abstract class AbstraktiBottiKuuntelija implements BottiKuuntelija {
+	protected Botti2 botti;
+	
+	public AbstraktiBottiKuuntelija( Botti2 botti ) {
+		this.botti = botti;
+	}
 	
 	public int annaPrioriteetti() {
 		return 0;
 	}
-	
 	public boolean uusiViesti( String viesti, String kanava, String lahettaja ) {
-		return true;
+		return false;
 	}
 	public void kanavallaUusiKayttaja( String nick ) {}
 	public void kanavaltaLahtiKayttaja( String nick ) {}
+	
+	public int compareTo( BottiKuuntelija vertailtava ) {
+		int omaPrioriteetti = this.annaPrioriteetti();
+		int vertailtavanPrioriteetti = vertailtava.annaPrioriteetti();
+		
+		if( omaPrioriteetti < vertailtavanPrioriteetti ) {
+			return -1;
+		} else if( omaPrioriteetti > vertailtavanPrioriteetti ) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
