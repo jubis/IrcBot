a1a418b3d283d3a74f70a91d01fb5bb82c86ca5a
Johannes Vainio <vainjo4@gmail.com>
2012-11-23 13:20:26 +0200

    lähettää nyt kuten esimerkkikuuntelija

--- src/Jtoiminto.java
+++ src/Jtoiminto.java
@@ -1,4 +1,4 @@
-import org.jibble.pircbot.PircBot;
+﻿import org.jibble.pircbot.PircBot;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,21 +7,76 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Jtoiminto implements BottiKuuntelija {
+//TODO : miten lähetysmetodia pitää kutsua? nyt toteutettu kuten EsimerkkiKuuntelija. PircBot-attribuutit kommenteissa.
 
+/**
+ * Jtoiminto tottelee komentoja 
+ * 
+ * "!wiki <sana>"
+ * 			tulostaa wikipediasta sanan tuottaman 
+ * 			artikkelin alusta < merkkimaara-attribuutin verran merkkeja.
+ * "!pituus <luku>"
+ * 			muuttaa attribuuttia merkkimaara
+ * 			
+ * @author Johannes
+ * @attribute merkkimaara	netista haettavan tekstin merkkien maara 
+ * @attribute pircbot 		Toiminnon tiedossa oleva PircBot
+ * 
+ * Lopullinen tuloste voi olla paljonkin lyhempi kuin merkkimaara, 
+ * silla haetusta tekstista poistetaan kaikki <p>:t, </b>:t yms.
+ * 
+ * Heitin prioriteetiksi 10.
+ */
+
+public class Jtoiminto implements BottiKuuntelija {
+	
 	/*
 	 * http://en.wikipedia.org/w/api.php
-	 * prop=extracts (ex) *
+	 * prop=extracts (ex)
 	 * Returns plain-text or limited HTML extracts of the given page(s)
 	 */
-	private static final String apiurl = "http://en.wikipedia.org/w/api.php?action=query&format=txt&prop=extracts&exchars=500&titles=";	
-
-	private static BufferedReader lukija = new BufferedReader(new 
+	private int merkkimaara = 500;	
+//	private PircBot pircbot;	
+	private HelppoBotti botti;
+	private BufferedReader lukija = new BufferedReader(new 
 			InputStreamReader(System.in));
 
-	public Jtoiminto(PircBot pircbot) {
+	
+	public Jtoiminto(PircBot pircbot, HelppoBotti botti) {
+	//this.pircbot = pircbot;
+	this.botti = botti;
 	}
+	
+	private String annaApiurl() {
+		return "http://en.wikipedia.org/w/api.php?action=query&format=txt&prop=extracts&exchars="+annaArtikkelinPituus()+"&titles=";
+	}
+	
+	private void asetaArtikkelinPituus(int merkit) {
+		merkkimaara = merkit;
+	}
+	
+	private String annaArtikkelinPituus() {
+		return ""+merkkimaara;
+	}
+	
+	
 
+	public boolean uusiViesti(String viesti, String kanava, String lahettaja) {
+		
+		//toimii kun/jos viesti muotoa "!wiki <aihe>":
+		//ymmartaa vain yksiosaisia hakusanoja.
+		
+		if (viesti.contains("!wiki")) {	
+			return logiikka(viesti, kanava);
+		}
+		
+		//artikkelin pituutta voidaan saataa "!pituus <luku>"
+		else if (viesti.contains("!pituus")) {
+			return kutsuAsetus(viesti, kanava);
+		}
+		else return false;
+	}
+	
 	private URL teeUrl(String raakaurl) {
 		try {
 			return new java.net.URL(raakaurl);
@@ -33,123 +88,136 @@ public class Jtoiminto implements BottiKuuntelija {
 
 	}
 
-	private boolean logiikka(String viesti) {
-		String[] menotaulu = viesti.split("\\s+");
-
-		String sana = null;
-		try {sana = menotaulu[1];
-		}	catch(ArrayIndexOutOfBoundsException ex) {
-			return false;
-		}
-
-		//teipataan url kasaan
-		URL url = teeUrl(apiurl + sana);
-
-		//tehdaan inputstream, jolla haetaan tavaraa netista
+	private InputStream avaaVirta(URL url) {
 		InputStream virta = null;
 		try {
 			virta = url.openStream();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return false;
+			return null;
 		} 
+		return virta;
+	}
+	
+	private boolean logiikka(String viesti, String kanava) {
+		String[] viestiosina = viesti.split("\\s+");
+		String hakusana = null;
+		
+		if (viestiosina.length == 2) {
+			hakusana = viestiosina[1];
+			
+		}	else return false;
+		
+		//teipataan url kasaan
+		URL url = teeUrl(annaApiurl() + hakusana);
+
+		//tehdaan inputstream, jolla haetaan tavaraa netista
+		InputStream virta = avaaVirta(url);
+	
+		if (virta == null) {
+			return false;
+		}
+		
 		//teetekstia() hakee streamin avulla tekstin
 		String teksti = teeTekstia(virta);
-
+		
+		//suljetaan virta
+		try {
+			virta.close();
+		} catch (IOException e) {}
 
 		//jaetaan teksti osiin [extract]-tagin ja "...":n mukaan
 		String[] paluutaulu = teksti.split("extract]|(\\.\\.\\.)");
 
 		//poistetaan tuloksesta <p>:t, </b>:t yms jne.
 		String tulos = null;
-		try{
+		if (paluutaulu.length > 1) {
 			tulos = paluutaulu[1].replaceAll("<.>","");
 			tulos = tulos.replaceAll("<..>","");
 			tulos = tulos.replaceAll("<...>","");
 			tulos = tulos.replaceAll("<....>","");
-		} catch(RuntimeException Re) {
+		} else {
 			System.out.println("virhe");
+			return false;
 		}
 		//null-tarkastus
 		if(tulos == null) {
 			return false;
 		}
+		//jos tuloksessa on sana REDIRECT, haku on palauttanut redirect-sivun
 		if(tulos.contains("REDIRECT")){
-			System.out.println("redirect-sivu");
+			System.out.println("palautti redirect-sivun, yrita muotoilla uudestaan");
 			return false;
 		}
 
-		//tyyliseikkana teipataan pisteet takaisin.
+		//liitetaan pisteet takaisin, jotka poistettiin paluutaulun splitissa
 		tulos += "...";
 
-		//printataan tulos
-		System.out.println(tulos);
-
-
-		//yritetään vielä sulkea virta
-		try {
-			virta.close();
-		} catch (IOException e) {}
-
+		//printataan / lahetetaan tulos
+		
+		//System.out.println(tulos);
+		this.botti.lahetaViesti(tulos, kanava);
 		return true;
 	}
 
-
-
-	public boolean uusiViesti(String viesti, String kanava, String lahettaja) {
-
-		//toimii kun/jos viesti muotoa "!wiki <aihe>":
-		//ymmartaa vain yksiosaisia hakusanoja.
-
-		if (viesti.contains("!wiki")) {	
-			return logiikka(viesti);
-		}
+	private boolean kutsuAsetus(String viesti, String kanava) {
+		String[] viestiosina = viesti.split("\\s+");
+		int merkit = 0;
+		
+		if (viestiosina.length == 2) {
+			
+			merkit = Integer.parseInt(viestiosina[1]);
+			
+			asetaArtikkelinPituus(merkit);
+		
+			//System.out.println("pituudeksi asetettu "+merkit+" merkkia.", kanava);
+			this.botti.lahetaViesti("pituudeksi asetettu "+merkit+" merkkia.", kanava);
+		return true;
+		}	
 		else return false;
 	}
+
 
 	private static String teeTekstia(InputStream virta) {
 		java.util.Scanner scan = new java.util.Scanner(virta).useDelimiter("//A");
 		return scan.hasNext() ? scan.next() : "";	
 	}
+	
+	
 	public int annaPrioriteetti() {
-		return 0;		
+		//heitin jotain.
+		return 10;		
 	}
 
 	public int compareTo(BottiKuuntelija arg0) {	
 		return 0;
 	}
 
-	public void kanavallaUusiKayttaja(String nick) {
-
-	}
-
-	public void kanavaltaLahtiKayttaja(String nick) {
-
-	}
-
-	private static String kysy() {
+	private String kysy() {
 		try {
-			return lukija.readLine();
+			return this.lukija.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	public static void main(String[] args) {
-		Jtoiminto J = new Jtoiminto(null);
+	public void kanavallaUusiKayttaja(String nick, String kanava) {
+				
+	}
 
-		System.out.println("mita haluat tietaa? (muista !wiki):");
+	public void kanavaltaLahtiKayttaja(String nick, String kanava) {
+	
+	}
+
+	public static void main(String[] args) {
+		
+		Jtoiminto J = new Jtoiminto(null, null);
+		System.out.println("mita haluat tehda? (!pituus / !wiki):");
 		while(true){
-			J.uusiViesti(kysy(), "", "");
+			J.uusiViesti(J.kysy(), "", "");
 		}
 	}
+	
 
-	@Override
-	public void kanavallaUusiKayttaja( String nick, String kanava ) {	
-	}
-
-	@Override
-	public void kanavaltaLahtiKayttaja( String nick, String kanava ) {
-	}
 }
